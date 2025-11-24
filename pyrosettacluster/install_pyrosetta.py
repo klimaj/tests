46af1c191728a7d18887d3da38d2e668d8ea0f13
Jason C. Klima <klimaj@uw.edu>
2025-11-24 14:57:42 -0800

    Skip installation with mirror=0

--- pyrosettacluster/install_pyrosetta.py
+++ pyrosettacluster/install_pyrosetta.py
@@ -5,7 +5,7 @@ import pyrosetta_installer
 
 
 # Order of mirrors to prioritize
-MIRROR_ORDER = (1, 0)
+MIRROR_ORDER = (1,) # (1, 0)
 
 
 kwargs = dict(
@@ -22,9 +22,10 @@ try:
     kwargs.update(mirror=MIRROR_ORDER[0])
     pyrosetta_installer.install_pyrosetta(**kwargs)
 except Exception as ex:
-    print(
-        f"PyRosetta installation with 'mirror={MIRROR_ORDER[0]}' failed: {ex}. "
-        f"Retrying with 'mirror={MIRROR_ORDER[1]}'."
-    )
-    kwargs.update(mirror=MIRROR_ORDER[1])
-    pyrosetta_installer.install_pyrosetta(**kwargs)
+    _err_msg = f"PyRosetta installation with 'mirror={MIRROR_ORDER[0]}' failed: {ex}"
+    if len(MIRROR_ORDER) == 2:
+        print(f"{_err_msg}. Retrying with 'mirror={MIRROR_ORDER[1]}'.")
+        kwargs.update(mirror=MIRROR_ORDER[1])
+        pyrosetta_installer.install_pyrosetta(**kwargs)
+    else:
+        raise NotImplementedError(f"{_err_msg} Skipping installation attempts with mirrors.")
